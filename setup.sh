307bd44406e9869a59fa37346cf6ecbed7e82724
nobu666 <nobutoshi.ogata@smartnews.com>
2015-07-22 13:25:35 +0900

    add p4merge script

--- setup.sh
+++ setup.sh
@@ -5,6 +5,8 @@ for file in $(find . -name '.*' | grep -v "^.$" | xargs basename)
 do
     ln -fs $(pwd)/${file} ${HOME}/${file}
 done
+sudo cp p4merge /usr/local/bin/p4merge
+sudo chmod +x /usr/local/bin/p4merge
 if [ ! -d ~/.vim/bundle ]
 then
     mkdir -p ~/.vim/bundle
