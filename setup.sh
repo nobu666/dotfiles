79af4ba9324faf52bd3aeffcff24190f9fce7540
nobu666 <nobutoshi.ogata@smartnews.com>
2016-06-16 14:47:07 +0900

    use compactionHeuristic diff

--- setup.sh
+++ setup.sh
@@ -5,6 +5,17 @@ for file in $(find . -name '.*' | grep -v "^.$" | xargs basename)
 do
     ln -fs $(pwd)/${file} ${HOME}/${file}
 done
+if ! PATH=~/bin:$PATH type -P diff-highlight >/dev/null 2>&1; then
+    (
+    for d in /usr/local/share /usr/share; do
+        if [[ -x $d/git-core/contrib/diff-highlight/diff-highlight ]]; then
+            [[ -d ~/bin ]] || mkdir ~/bin
+            ln -sfn "$d/git-core/contrib/diff-highlight/diff-highlight" ~/bin/
+            exit
+        fi
+    done
+    )
+fi
 sudo cp p4merge /usr/local/bin/p4merge
 sudo chmod +x /usr/local/bin/p4merge
 if [ ! -d ~/.vim/bundle ]
